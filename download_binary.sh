a9b985df9a1a1aa7695cee19ec627fb55086efcd
李永健 <liyongjian@9fbank.com.cn>
2020-06-23 15:36:38 +0800

    add etcd function

--- download_binary.sh
+++ download_binary.sh
@@ -5,50 +5,90 @@ CNI_PLUGIN_VER=0.8.6
 ETCD_VER=3.4.9
 K8S_SERVER_VER=1.18.3
 FLANNEL_VER=0.12.0
-mkdir /opt/pkg && cd /opt/pkg && \
-wget https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz && \
-wget https://github.com/coreos/flannel/releases/download/v${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz && \
-wget https://dl.k8s.io/v${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz && \
-wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VER}/etcd-v${ETCD_VER}-linux-amd64.tar.gz
 
+mkdir /opt/pkg 
+cd /opt/pkg 
+#wget https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz && \
+#wget https://github.com/coreos/flannel/releases/download/v${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz && \
+#wget https://dl.k8s.io/v${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz && \
+#wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VER}/etcd-v${ETCD_VER}-linux-amd64.tar.gz && \
+#wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 && \
+#wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 && \
+#wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 
 
-
-if [ -f ./cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
-    mkdir -p cni/cni-${CNI_PLUGIN_VER}
-    mv cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ./cni/cni-${CNI_PLUGIN_VER}/ && \
-    cd ./cni/cni-${CNI_PLUGIN_VER}/ && \
-    tar xf cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz
-    cd -
+if [ -f ./cni-plugins/${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
+    echo "[INFO] cni-plugins 已存在"
 else
-    echo "cni-plugin 二进制包不存在"
+    wget https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz 
+
+    if [ -f ./cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
+        mkdir -p cni-plugins/${CNI_PLUGIN_VER}
+        mv cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ./cni-plugins/${CNI_PLUGIN_VER}/ && \
+        cd ./cni-plugins/${CNI_PLUGIN_VER}/ && \
+        tar xf cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz
+        echo "[INFO] 下载 cni-plgins 并解压完成"
+        cd - &>/dev/null
+    fi
 fi
 
-if [ -f ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
-    mkdir -p flannel/flannel-${FLANNEL_VER}
-    mv ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ./flannel/flannel-${FLANNEL_VER}/ && \
-    cd ./flannel/flannel-${FLANNEL_VER}/ && \
-    tar xf flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
-    cd -
+if [ -f ./flannel/${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
+    echo "[INFO] flannel 已存在"
 else
-    echo "flannel 二进制包不存在"
+    wget https://github.com/coreos/flannel/releases/download/v${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
+
+    if [ -f ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
+        mkdir -p flannel/${FLANNEL_VER}
+        mv ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ./flannel/${FLANNEL_VER}/ && \
+        cd ./flannel/${FLANNEL_VER}/ && \
+        tar xf flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
+        echo "[INFO] 下载 flannel 并解压完成"
+        cd - &>/dev/null
+    fi
 fi
 
-if [ -f ./kubernetes-server-linux-amd64.tar.gz ];then
-    mkdir k8s-${K8S_SERVER_VER}
-    mv kubernetes-server-linux-amd64.tar.gz ./k8s-${K8S_SERVER_VER}/ && \
-    cd ./k8s-${K8S_SERVER_VER}/ && \
-    tar xf kubernetes-server-linux-amd64.tar.gz
-    cd -
+if [ -f ./k8s/${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz  ];then
+    echo "[INFO] k8s server 已存在"
 else
-    echo "k8s 二进制包不存在"
+    wget https://dl.k8s.io/v${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz 
+
+    if [ -f ./kubernetes-server-linux-amd64.tar.gz ];then
+        mkdir -p k8s/${K8S_SERVER_VER}
+        mv kubernetes-server-linux-amd64.tar.gz ./k8s/${K8S_SERVER_VER}/ && \
+        cd ./k8s/${K8S_SERVER_VER}/ && \
+        tar xf kubernetes-server-linux-amd64.tar.gz
+        echo "[INFO] 下载 k8s-server 并解压完成"
+        cd - &>/dev/null
+    fi
 fi
 
-if [ -f etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
-    mkdir -p etcd
-    mv etcd-v${ETCD_VER}-linux-amd64.tar.gz ./etcd/ && \
-    cd ./etcd/ && \
-    tar xf etcd-v${ETCD_VER}-linux-amd64.tar.gz
-    cd -
+if [ -f ./etcd/etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
+    echo "[INFO] etcd 已存在"
 else
-    echo "etcd 二进制包不存在"
+    wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VER}/etcd-v${ETCD_VER}-linux-amd64.tar.gz 
+
+    if [ -f etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
+        mkdir -p etcd
+        mv etcd-v${ETCD_VER}-linux-amd64.tar.gz ./etcd/ && \
+        cd ./etcd/ && \
+        tar xf etcd-v${ETCD_VER}-linux-amd64.tar.gz
+        echo "[INFO] 下载 etcd 并解压完成"
+        cd - &>/dev/null
+    fi
+fi
+
+if [ -f ./cfssl/cfssl_linux-amd64 ];then
+    echo "[INFO] cfssl 已存在"
+else
+    wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 && \
+    wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 && \
+    wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 
+
+    if [ -f cfssl_linux-amd64 ];then
+        mkdir -p cfssl 
+        mv cfssl_linux-amd64 ./cfssl/ && \
+        mv cfssljson_linux-amd64 ./cfssl/ && \
+        mv cfssl-certinfo_linux-amd64 ./cfssl/ 
+        echo "[INFO] 下载 cfssl 相关包完成"
+        cd - &>/dev/null
+    fi 
 fi
---
+++ tools/move_pkg.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CNI_PLUGIN_VER=0.8.6
+ETCD_VER=3.4.9
+K8S_SERVER_VER=1.18.3
+FLANNEL_VER=0.12.0
+
+cd /opt/pkg/
+
+if [ -f ./cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
+    mkdir -p cni-plugins/${CNI_PLUGIN_VER}
+    mv cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ./cni-plugins/${CNI_PLUGIN_VER}/ && \
+    cd ./cni-plugins/${CNI_PLUGIN_VER}/ && \
+    tar xf cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz
+    echo "[INFO] 下载 cni-plgins 并解压完成"
+    cd - &>/dev/null
+fi
+if [ -f ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
+    mkdir -p flannel/${FLANNEL_VER}
+    mv ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ./flannel/${FLANNEL_VER}/ && \
+    cd ./flannel/${FLANNEL_VER}/ && \
+    tar xf flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
+    echo "[INFO] 下载 flannel 并解压完成"
+    cd - &>/dev/null
+fi
+if [ -f ./kubernetes-server-linux-amd64.tar.gz ];then
+    mkdir -p k8s/${K8S_SERVER_VER}
+    mv kubernetes-server-linux-amd64.tar.gz ./k8s/${K8S_SERVER_VER}/ && \
+    cd ./k8s/${K8S_SERVER_VER}/ && \
+    tar xf kubernetes-server-linux-amd64.tar.gz
+    echo "[INFO] 下载 k8s-server 并解压完成"
+    cd - &>/dev/null
+fi
+if [ -f etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
+    mkdir -p etcd
+    mv etcd-v${ETCD_VER}-linux-amd64.tar.gz ./etcd/ && \
+    cd ./etcd/ && \
+    tar xf etcd-v${ETCD_VER}-linux-amd64.tar.gz
+    echo "[INFO] 下载 etcd 并解压完成"
+    cd - &>/dev/null
+fi
+if [ -f cfssl_linux-amd64 ];then
+    mkdir -p cfssl 
+    mv cfssl_linux-amd64 ./cfssl/ && \
+    mv cfssljson_linux-amd64 ./cfssl/ && \
+    mv cfssl-certinfo_linux-amd64 ./cfssl/ 
+    echo "[INFO] 下载 cfssl 相关包完成"
+    cd - &>/dev/null
+fi 

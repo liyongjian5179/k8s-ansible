3ece77ee535287489570f957743dc06c38fd5c5e
李永健 <liyongjian@9fbank.com.cn>
2020-06-24 16:59:53 +0800

    fix clean.sh

--- tools/clean.sh
+++ tools/clean.sh
@@ -8,7 +8,7 @@ ansible -i ../invertory/hosts k8s -m systemd -a 'name=kube-proxy state=stopped e
 ansible -i ../invertory/hosts k8s -m systemd -a 'name=flanneld state=stopped enabled=no'
 ansible -i ../invertory/hosts k8s -m systemd -a 'name=docker state=stopped enabled=no'
 ansible -i ../invertory/hosts k8s -m yum -a 'name=docker state=absent'
-ansible -i ../invertory/hosts all -m shell -a 'rm -rf /opt/kubernetes /opt/etcd /var/run/flannel /var/lib/kubelet /etc/docker /var/lib/docker'
+ansible -i ../invertory/hosts all -m shell -a 'rm -rf /opt/kubernetes /opt/etcd /opt/cni /etc/cni /var/run/flannel /var/lib/kubelet /etc/docker /var/lib/docker'
 ansible -i ../invertory/hosts all -m shell -a "rm -f /usr/lib/systemd/system/{docker,flanneld,kubelet,kube-proxy,kube-apiserver,kube-controller-manager,kube-scheduler}.service"
 ansible -i ../invertory/hosts all -m shell -a 'ip link set dev cni0 down; ip line set dev docker0 down'
 ansible -i ../invertory/hosts all -m shell -a 'ip link delete cni0; ip link delete docker0'

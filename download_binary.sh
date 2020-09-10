2013286f32d3e0b5d0bc5345900057b781d4d141
李永健 <liyongjian5179@163.com>
2020-09-10 11:47:49 +0800

    下载脚本使用统一配置文件

--- download_binary.sh
+++ download_binary.sh
@@ -1,92 +1,99 @@
 #!/bin/bash
 # Download
 
-CNI_PLUGIN_VER=0.8.6
-ETCD_VER=3.4.9
-K8S_SERVER_VER=1.18.3
-FLANNEL_VER=0.12.0
-CALICO_VER=3.15.0
+#cni_plugins_version=0.8.6
+#etcd_version=3.4.9
+#kubernetes_version=1.18.3
+#flannel_version=0.12.0
+#calico_version=3.15.0
 
-mkdir /opt/pkg 
-cd /opt/pkg 
-#wget https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz && \
-#wget https://github.com/coreos/flannel/releases/download/v${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz && \
-#wget https://dl.k8s.io/v${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz && \
-#wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VER}/etcd-v${ETCD_VER}-linux-amd64.tar.gz && \
+cni_plugins_version=$(awk '/^cni_plugins_version/ {print $2}' ./group_vars/all)
+etcd_version=$(awk '/^etcd_version/ {print $2}' ./group_vars/all)
+kubernetes_version=$(awk '/^etcd_version/ {print $2}' ./group_vars/all)
+flannel_version=$(awk '/^flannel_version/ {print $2}' ./group_vars/all)
+calico_version=$(awk '/^calico_version/ {print $2}' ./group_vars/all)
+network_type=$(awk '/^network_type/ {print $2}' ./group_vars/all)
+
+mkdir /opt/pkg
+cd /opt/pkg
+#wget https://github.com/containernetworking/plugins/releases/download/v${cni_plugins_version}/cni-plugins-linux-amd64-v${cni_plugins_version}.tgz && \
+#wget https://github.com/coreos/flannel/releases/download/v${flannel_version}/flannel-v${flannel_version}-linux-amd64.tar.gz && \
+#wget https://dl.k8s.io/v${kubernetes_version}/kubernetes-server-linux-amd64.tar.gz && \
+#wget https://github.com/etcd-io/etcd/releases/download/v${etcd_version}/etcd-v${etcd_version}-linux-amd64.tar.gz && \
 #wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 && \
 #wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 && \
 #wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64  &&\
-#wget https://github.com/projectcalico/calicoctl/releases/download/v{CALICO_VER}/calicoctl
+#wget https://github.com/projectcalico/calicoctl/releases/download/v{calico_version}/calicoctl
 
-if [ -f ./cni-plugins/${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
+if [ -f ./cni-plugins/${cni_plugins_version}/cni-plugins-linux-amd64-v${cni_plugins_version}.tgz ];then
     echo "[INFO] cni-plugins 已存在"
 else
-    wget https://github.com/containernetworking/plugins/releases/download/v${CNI_PLUGIN_VER}/cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz 
+    wget https://github.com/containernetworking/plugins/releases/download/v${cni_plugins_version}/cni-plugins-linux-amd64-v${cni_plugins_version}.tgz
 
-    if [ -f ./cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
-        mkdir -p cni-plugins/${CNI_PLUGIN_VER}
-        mv cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ./cni-plugins/${CNI_PLUGIN_VER}/ && \
-        cd ./cni-plugins/${CNI_PLUGIN_VER}/ && \
-        tar xf cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz
+    if [ -f ./cni-plugins-linux-amd64-v${cni_plugins_version}.tgz ];then
+        mkdir -p cni-plugins/${cni_plugins_version}
+        mv cni-plugins-linux-amd64-v${cni_plugins_version}.tgz ./cni-plugins/${cni_plugins_version}/ && \
+        cd ./cni-plugins/${cni_plugins_version}/ && \
+        tar xf cni-plugins-linux-amd64-v${cni_plugins_version}.tgz
         echo "[INFO] 下载 cni-plgins 并解压完成"
         cd - &>/dev/null
     fi
 fi
 
-if [ -f ./flannel/${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
+if [ -f ./flannel/${flannel_version}/flannel-v${flannel_version}-linux-amd64.tar.gz ];then
     echo "[INFO] flannel 已存在"
 else
-    wget https://github.com/coreos/flannel/releases/download/v${FLANNEL_VER}/flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
+    wget https://github.com/coreos/flannel/releases/download/v${flannel_version}/flannel-v${flannel_version}-linux-amd64.tar.gz
 
-    if [ -f ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
-        mkdir -p flannel/${FLANNEL_VER}
-        mv ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ./flannel/${FLANNEL_VER}/ && \
-        cd ./flannel/${FLANNEL_VER}/ && \
-        tar xf flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
+    if [ -f ./flannel-v${flannel_version}-linux-amd64.tar.gz ];then
+        mkdir -p flannel/${flannel_version}
+        mv ./flannel-v${flannel_version}-linux-amd64.tar.gz ./flannel/${flannel_version}/ && \
+        cd ./flannel/${flannel_version}/ && \
+        tar xf flannel-v${flannel_version}-linux-amd64.tar.gz
         echo "[INFO] 下载 flannel 并解压完成"
         cd - &>/dev/null
     fi
 fi
 
-if [ -f ./calico/${CALICO_VER}/calicoctl ];then
+if [ -f ./calico/${calico_version}/calicoctl ];then
     echo "[INFO] calicoctl 已存在"
 else
-    wget https://github.com/projectcalico/calicoctl/releases/download/v${CALICO_VER}/calicoctl
+    wget https://github.com/projectcalico/calicoctl/releases/download/v${calico_version}/calicoctl
 
     if [ -f ./calicoctl ];then
-        mkdir -p calico/${CALICO_VER}
-        mv ./calicoctl ./calico/${CALICO_VER}/ && \
-        cd ./calico/${CALICO_VER}/ && \
+        mkdir -p calico/${calico_version}
+        mv ./calicoctl ./calico/${calico_version}/ && \
+        cd ./calico/${calico_version}/ && \
         echo "[INFO] 下载 calicoctl 完成"
         cd - &>/dev/null
     fi
 fi
 
-if [ -f ./k8s/${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz  ];then
+if [ -f ./k8s/${kubernetes_version}/kubernetes-server-linux-amd64.tar.gz  ];then
     echo "[INFO] k8s server 已存在"
 else
-    wget https://dl.k8s.io/v${K8S_SERVER_VER}/kubernetes-server-linux-amd64.tar.gz 
+    wget https://dl.k8s.io/v${kubernetes_version}/kubernetes-server-linux-amd64.tar.gz
 
     if [ -f ./kubernetes-server-linux-amd64.tar.gz ];then
-        mkdir -p k8s/${K8S_SERVER_VER}
-        mv kubernetes-server-linux-amd64.tar.gz ./k8s/${K8S_SERVER_VER}/ && \
-        cd ./k8s/${K8S_SERVER_VER}/ && \
+        mkdir -p k8s/${kubernetes_version}
+        mv kubernetes-server-linux-amd64.tar.gz ./k8s/${kubernetes_version}/ && \
+        cd ./k8s/${kubernetes_version}/ && \
         tar xf kubernetes-server-linux-amd64.tar.gz
         echo "[INFO] 下载 k8s-server 并解压完成"
         cd - &>/dev/null
     fi
 fi
 
-if [ -f ./etcd/etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
+if [ -f ./etcd/etcd-v${etcd_version}-linux-amd64.tar.gz ];then
     echo "[INFO] etcd 已存在"
 else
-    wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VER}/etcd-v${ETCD_VER}-linux-amd64.tar.gz 
+    wget https://github.com/etcd-io/etcd/releases/download/v${etcd_version}/etcd-v${etcd_version}-linux-amd64.tar.gz
 
-    if [ -f etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
+    if [ -f etcd-v${etcd_version}-linux-amd64.tar.gz ];then
         mkdir -p etcd
-        mv etcd-v${ETCD_VER}-linux-amd64.tar.gz ./etcd/ && \
+        mv etcd-v${etcd_version}-linux-amd64.tar.gz ./etcd/ && \
         cd ./etcd/ && \
-        tar xf etcd-v${ETCD_VER}-linux-amd64.tar.gz
+        tar xf etcd-v${etcd_version}-linux-amd64.tar.gz
         echo "[INFO] 下载 etcd 并解压完成"
         cd - &>/dev/null
     fi
@@ -97,14 +104,14 @@ if [ -f ./cfssl/cfssl_linux-amd64 ];then
 else
     wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 && \
     wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 && \
-    wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64 
+    wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
 
     if [ -f cfssl_linux-amd64 ];then
-        mkdir -p cfssl 
+        mkdir -p cfssl
         mv cfssl_linux-amd64 ./cfssl/ && \
         mv cfssljson_linux-amd64 ./cfssl/ && \
-        mv cfssl-certinfo_linux-amd64 ./cfssl/ 
+        mv cfssl-certinfo_linux-amd64 ./cfssl/
         echo "[INFO] 下载 cfssl 相关包完成"
         cd - &>/dev/null
-    fi 
+    fi
 fi
--- tools/move_pkg.sh
+++ tools/move_pkg.sh
@@ -1,57 +1,64 @@
 #!/bin/bash
 
-CNI_PLUGIN_VER=0.8.6
-ETCD_VER=3.4.9
-K8S_SERVER_VER=1.18.3
-FLANNEL_VER=0.12.0
-CALICO_VER=3.15.0
+#cni_plugins_version=0.8.6
+#etcd_version=3.4.9
+#kubernetes_version=1.18.3
+#flannel_version=0.12.0
+#calico_version=3.15.0
+
+cni_plugins_version=$(awk '/^cni_plugins_version/ {print $2}' ./group_vars/all)
+etcd_version=$(awk '/^etcd_version/ {print $2}' ./group_vars/all)
+kubernetes_version=$(awk '/^kubernetes_version/ {print $2}' ./group_vars/all)
+flannel_version=$(awk '/^flannel_version/ {print $2}' ./group_vars/all)
+calico_version=$(awk '/^calico_version/ {print $2}' ./group_vars/all)
+network_type=$(awk '/^network_type/ {print $2}' ./group_vars/all)
 
 cd /opt/pkg/
 
-if [ -f ./cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ];then
-    mkdir -p cni-plugins/${CNI_PLUGIN_VER}
-    mv cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz ./cni-plugins/${CNI_PLUGIN_VER}/ && \
-    cd ./cni-plugins/${CNI_PLUGIN_VER}/ && \
-    tar xf cni-plugins-linux-amd64-v${CNI_PLUGIN_VER}.tgz
+if [ -f ./cni-plugins-linux-amd64-v${cni_plugins_version}.tgz ];then
+    mkdir -p cni-plugins/${cni_plugins_version}
+    mv cni-plugins-linux-amd64-v${cni_plugins_version}.tgz ./cni-plugins/${cni_plugins_version}/ && \
+    cd ./cni-plugins/${cni_plugins_version}/ && \
+    tar xf cni-plugins-linux-amd64-v${cni_plugins_version}.tgz
     echo "[INFO] 下载 cni-plgins 并解压完成"
     cd - &>/dev/null
 fi
-if [ -f ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ];then
-    mkdir -p flannel/${FLANNEL_VER}
-    mv ./flannel-v${FLANNEL_VER}-linux-amd64.tar.gz ./flannel/${FLANNEL_VER}/ && \
-    cd ./flannel/${FLANNEL_VER}/ && \
-    tar xf flannel-v${FLANNEL_VER}-linux-amd64.tar.gz
+if [ -f ./flannel-v${flannel_version}-linux-amd64.tar.gz ];then
+    mkdir -p flannel/${flannel_version}
+    mv ./flannel-v${flannel_version}-linux-amd64.tar.gz ./flannel/${flannel_version}/ && \
+    cd ./flannel/${flannel_version}/ && \
+    tar xf flannel-v${flannel_version}-linux-amd64.tar.gz
     echo "[INFO] 下载 flannel 并解压完成"
     cd - &>/dev/null
 fi
 if [ -f ./calicoctl ];then
-    mkdir -p calico/${CALICO_VER}
-    mv ./calicoctl ./calico/${CALICO_VER}/ && \
-    cd ./calico/${CALICO_VER}/ && \
+    mkdir -p calico/${calico_version}
+    mv ./calicoctl ./calico/${calico_version}/ && \
+    cd ./calico/${calico_version}/ && \
     echo "[INFO] 下载 calicoctl 完成"
     cd - &>/dev/null
 fi
 if [ -f ./kubernetes-server-linux-amd64.tar.gz ];then
-    mkdir -p k8s/${K8S_SERVER_VER}
-    mv kubernetes-server-linux-amd64.tar.gz ./k8s/${K8S_SERVER_VER}/ && \
-    cd ./k8s/${K8S_SERVER_VER}/ && \
+    mkdir -p k8s/${kubernetes_version}
+    mv kubernetes-server-linux-amd64.tar.gz ./k8s/${kubernetes_version}/ && \
+    cd ./k8s/${kubernetes_version}/ && \
     tar xf kubernetes-server-linux-amd64.tar.gz
     echo "[INFO] 下载 k8s-server 并解压完成"
     cd - &>/dev/null
 fi
-if [ -f etcd-v${ETCD_VER}-linux-amd64.tar.gz ];then
+if [ -f etcd-v${etcd_version}-linux-amd64.tar.gz ];then
     mkdir -p etcd
-    mv etcd-v${ETCD_VER}-linux-amd64.tar.gz ./etcd/ && \
+    mv etcd-v${etcd_version}-linux-amd64.tar.gz ./etcd/ && \
     cd ./etcd/ && \
-    tar xf etcd-v${ETCD_VER}-linux-amd64.tar.gz
+    tar xf etcd-v${etcd_version}-linux-amd64.tar.gz
     echo "[INFO] 下载 etcd 并解压完成"
     cd - &>/dev/null
 fi
 if [ -f cfssl_linux-amd64 ];then
-    mkdir -p cfssl 
+    mkdir -p cfssl
     mv cfssl_linux-amd64 ./cfssl/ && \
     mv cfssljson_linux-amd64 ./cfssl/ && \
-    mv cfssl-certinfo_linux-amd64 ./cfssl/ 
+    mv cfssl-certinfo_linux-amd64 ./cfssl/
     echo "[INFO] 下载 cfssl 相关包完成"
     cd - &>/dev/null
-fi 
+fi

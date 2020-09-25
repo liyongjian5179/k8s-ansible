67208c3c5c46348020d85d7478222059627f9f60
李永健 <liyongjian5179@163.com>
2020-09-25 18:35:42 +0800

    自定义 kubelet 存储位置

--- tools/clean.sh
+++ tools/clean.sh
@@ -10,9 +10,11 @@ ansible -i ./inventory/hosts k8s -m systemd -a 'name=docker state=stopped enable
 ansible -i ./inventory/hosts k8s -m yum -a 'name=docker-ce state=absent'
 ansible -i ./inventory/hosts k8s -m yum -a 'name=docker-ce-cli state=absent'
 ansible -i ./inventory/hosts masters -m shell -a 'mv -f /root/.kube/config /tmp/'
-ansible -i ./inventory/hosts k8s -m shell -a 'rm -rf /opt/kubernetes /opt/etcd /opt/cni /etc/cni /var/run/calico /etc/calico /var/run/flannel /opt/yamls /var/lib/kubelet /etc/docker /var/lib/docker /opt/docker /var/lib/dockershim'
+# 解绑 tmpfs 的挂载点
+ansible -i inventory/hosts k8s  -m raw -a "umount \$(df -HT | grep '/opt/kubelet/pods' | awk '{print \$7}')"
+ansible -i ./inventory/hosts k8s -m shell -a 'rm -rf /opt/kubernetes /opt/etcd /opt/cni /etc/cni /var/run/calico /etc/calico /var/run/flannel /opt/yamls /var/lib/kubelet /etc/docker /var/lib/docker /opt/docker /var/lib/dockershim /opt/kubelet'
 ansible -i ./inventory/hosts k8s -m shell -a "rm -f /usr/lib/systemd/system/{docker,etcd,flanneld,kubelet,kube-proxy,kube-apiserver,kube-controller-manager,kube-scheduler}.service"
-ansible -i ./inventory/hosts k8s -m shell -a 'ip link set dev cni0 down; ip link set dev docker0 down; ip link set dev flannel.1'
+ansible -i ./inventory/hosts k8s -m shell -a 'ip link set dev cni0 down; ip link set dev docker0 down; ip link set dev flannel.1 down'
 ansible -i ./inventory/hosts k8s -m shell -a 'ip link delete cni0; ip link delete docker0; ip link delete flannel.1'
 ansible -i ./inventory/hosts k8s -m shell -a 'ipvsadm -C && iptables -F'
 ansible -i ./inventory/hosts k8s -m shell -a 'modprobe -r ipip'
